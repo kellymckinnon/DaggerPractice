1e347b0e668034540c670d3a90398f3f507e13e7
kellymckinnon <kellymckinnon@utexas.edu>
2020-04-28 02:54:43 -0700

    add log out to toolbar

--- app/src/main/java/me/kellymckinnon/daggerpractice/ui/main/MainActivity.java
+++ app/src/main/java/me/kellymckinnon/daggerpractice/ui/main/MainActivity.java
@@ -1,6 +1,10 @@
 package me.kellymckinnon.daggerpractice.ui.main;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import me.kellymckinnon.daggerpractice.BaseActivity;
 import me.kellymckinnon.daggerpractice.R;
@@ -13,5 +17,23 @@ public class MainActivity extends BaseActivity {
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.main_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.logout:
+        sessionManager.logOut();
+        return true;
+    }
+
+    return super.onOptionsItemSelected(item);
   }
 }
