6478b5d4e2155e8f567c7b13a72b48d2df456219
kellymckinnon <kellymckinnon@utexas.edu>
2020-05-06 21:18:54 -0700

    add custom scopes

--- app/src/main/java/me/kellymckinnon/daggerpractice/di/ActivityBuildersModule.java
+++ app/src/main/java/me/kellymckinnon/daggerpractice/di/ActivityBuildersModule.java
@@ -3,9 +3,11 @@ package me.kellymckinnon.daggerpractice.di;
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
 import me.kellymckinnon.daggerpractice.di.auth.AuthModule;
+import me.kellymckinnon.daggerpractice.di.auth.AuthScope;
 import me.kellymckinnon.daggerpractice.di.auth.AuthViewModelsModule;
 import me.kellymckinnon.daggerpractice.di.main.MainFragmentBuildersModule;
 import me.kellymckinnon.daggerpractice.di.main.MainModule;
+import me.kellymckinnon.daggerpractice.di.main.MainScope;
 import me.kellymckinnon.daggerpractice.di.main.MainViewModelsModule;
 import me.kellymckinnon.daggerpractice.ui.auth.AuthActivity;
 import me.kellymckinnon.daggerpractice.ui.main.MainActivity;
@@ -13,9 +15,11 @@ import me.kellymckinnon.daggerpractice.ui.main.MainActivity;
 @Module
 public abstract class ActivityBuildersModule {
 
+  @AuthScope
   @ContributesAndroidInjector(modules = {AuthViewModelsModule.class, AuthModule.class})
   abstract AuthActivity contributeAuthActivity();
 
+  @MainScope
   @ContributesAndroidInjector(modules = {MainViewModelsModule.class,
       MainFragmentBuildersModule.class,
       MainModule.class})
